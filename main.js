e654bcf02be18c40f439536069d34318478658a4
rajesh662 <rajkuttyrajesh56@gmail.com>
2023-02-15 11:48:36 +0530

    upload

---
+++ main.js
@@ -0,0 +1,299 @@
+let foods =[
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/szmqe2zdhzmu3thhxpmb",
+        heading:"Ashirwad hotel and Restaurant",
+        para:"Chinese, Biryani Dibrugarh <br>,Chowkidingee",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a href="#">29 MINS</a>',
+        price:'<a href="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/gmd4irdeuaynsfvhzka7",
+        heading:"Hotel Shree Maya",
+        para:"North Indian, Thalis Dhar, Mohan Talkies",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a hraf="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ib7em4sq4adp5t83dmlo",
+        heading:" Fair Fresh Bakery",
+        para:"Bakery, Pizzas Central Dholpur, Central Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a hraf="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qlzxyotolz856exvjg8a",
+        heading:"Beast Cafe",
+        para:"Indian Ghanta Ghar, Central Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/nvgnteyufihhnbjav61v",
+        heading:"Pandit Sweets",
+        para:"Sweets, Snacks Ayodhya Kunj, Centarl Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/c362d9f9547d22be3aa521ca428a1894",
+        heading:"RJ 11 Cafe",
+        para:"Restaurant UnserviceableFast Food, ItalianAyodhya Kunj, Centarl Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/q6aqymasf9idqcvq2a0n",
+        heading:"R B Palace",
+        para:"Restaurant UnserviceableNorth Indian, Chinese A B road, Central Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/yc6dedudtamj3mcvfa78",
+        heading:"The Park",
+        para:"Restaurant UnserviceableNorth Indian, Chinese Ayodhya Kunj, Centarl Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/cadf6bcafa0befc96c8a6ea203a1fd64",
+        heading:"Bachelor's Cafe",
+        para:"Restaurant UnserviceableCafe, Fast Food Saipau Road, Central Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/2681e54ef580a6f5659071e884edb72a",
+        heading:"Pizza Hot",
+        para:"Restaurant UnserviceableNorth Indian, Chinese",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/c6c6d272286886736a81585161342f45",
+        heading:"Kenth Hari Resort",
+        para:"Restaurant UnserviceableNorth Indian, Chinese",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/qdusjvrnogi8ozvf6vzm",
+        heading:"Pizza Fire",
+        para:"Opens next at 11 am, todayPizzas, Pastas Central Dholpur, Central Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/hhbmohimhugnqfel7iu8",
+        heading:"Priyanka Hotel & Restaurant",
+        para:"Opens next at 11 am, todayNorth Indian Central Dholpur, Central Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/9052c13460596e02a648bf924a3a0790",
+        heading:"Nisha Egg Centre",
+        para:"Restaurant Unserviceable Indian, Snacks Raj niwas palece, Central Dholpur ",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/404a0bb9c5d015c0ed9ffa9911691e5a",
+        heading:"Vaibhav Cafe",
+        para:"Restaurant UnserviceableNorth Indian, Chinese Asha Hotel building, Central Dholpur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/d7cdeead85c0c0fa127c18f38f3c87a7",
+        heading:"Bakers Villa",
+        para:"Restaurant UnserviceableBakery, Pizzas Adarsh Puram Colony, Gola Ka Mandir",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+
+    },
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/gqoypm1urn234yhmdcuk",
+        heading:"TEA LOBBY CAFE",
+        para:"Restaurant UnserviceableBakery, Beverages  Gola Ka Mandir, Thatipur",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/25a7b1e2871425c613a0143ac0155a1d",
+        heading:"Bansiwala - Tansen Road",
+        para:"Restaurant UnserviceableNorth Indian, Beverages R.P .Colony Tansen Road, Lashkar",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+
+    },
+    {
+        img: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/culjm05l9m3ptlqgclcz",
+        heading:"Bansiwala - Tansen Road",
+        para:"Restaurant UnserviceableNorth Indian, Beverages R.P .Colony Tansen Road, Lashkar",
+        rating:'<a href="#"><i class="fa-solid fa-star"> 3.9</i></a>',
+        mins:'<a hraf="#">29 MINS</a>',
+        price:'<a hraf="#"> $200 FOR TWO</a>',
+        offer:'<a href="#"><i class="fa-solid fa-user"></i> FLAT100 off | Use FLATDEAL</a>',
+        ext:'<a href="#">QUICK VIEW</a>',
+    },
+]
+
+
+
+let sect = document.createElement("section");
+document.body.appendChild(sect);
+
+let div1 = document.createElement("div");
+div1.classList.add("container")
+sect.appendChild(div1);
+
+let div2 = document.createElement("div");
+div2.classList.add("row");
+div1.appendChild(div2);
+
+foods.forEach(function(rk){
+
+
+
+let div3 = document.createElement("div");
+div3.classList.add("col-3");
+div3.classList.add("gx-5");
+div2.appendChild(div3);
+
+let img = document.createElement("img");
+img.src = rk.img;
+div3.appendChild(img);
+
+let heading = document.createElement("h4");
+heading.innerHTML=rk.heading;
+heading.classList.add("pt-3");
+div3.appendChild(heading);
+
+let logcal = document.createElement("p");
+logcal.innerHTML=rk.para;
+div3.appendChild(logcal);
+
+let div4 = document.createElement("div");
+div4.classList.add("d-flex");
+div4.classList.add("pb-2");
+div3.appendChild(div4);
+
+let div5 = document.createElement("div");
+div5.innerHTML=rk.rating;
+div5.classList.add("flex-grow-1");
+div4.appendChild(div5);
+
+let  div6 =document.createElement("div");
+div6.classList.add("flex-grow-1");
+
+div6.innerHTML = rk.mins;
+div4.appendChild(div6);
+ 
+let div7 = document.createElement("div");
+div7.innerHTML = rk.price;
+div7.classList.add("flex-grow-1");
+div4.appendChild(div7);
+
+let div8 = document.createElement("div");
+div8.innerHTML = rk.offer;
+div8.classList.add("li-handle");
+div3.appendChild(div8);
+
+let div9 = document.createElement("div");
+div9.classList.add("card-quick");
+div9.innerHTML = rk.ext;
+div3.appendChild(div9);
+
+
+});
+
+console.log(sect);
